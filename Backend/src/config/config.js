fdc8fe18d7f1f7b84e6b4321e9d887a99666832d
nichan said <said.nichan14@gmail.com>
2025-05-05 19:24:08 +0100

    version fonctionnel

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -3,14 +3,14 @@
 export default {
   development: {
     username: 'postgres',
-    password: 'said@@2022',
+    password: 'said',
     database: 'evalydb',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
   test: {
     username: 'postgres',
-    password: 'said@@2022',
+    password: 'said',
     database: 'evalydb',
     host: '127.0.0.1',
     dialect: 'postgres',
